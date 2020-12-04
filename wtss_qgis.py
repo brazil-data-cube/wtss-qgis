3b9351c03eaa6ccea74660ee09be830982bab2a5
AbnerErnaniADSFatec <abiner.anjos@gmail.com>
2020-12-04 11:12:41 -0300

    remove unused method

--- wtss_qgis.py
+++ wtss_qgis.py
@@ -43,18 +43,6 @@ from .wtss_plugin.wtss_qgis_controller import Controls, Services
 # Import the code for the dialog
 from .wtss_qgis_dialog import wtss_qgisDialog
 
-class color:
-    PURPLE = '\033[95m'
-    CYAN = '\033[96m'
-    DARKCYAN = '\033[36m'
-    BLUE = '\033[94m'
-    GREEN = '\033[92m'
-    YELLOW = '\033[93m'
-    RED = '\033[91m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-    END = '\033[0m'
-
 class wtss_qgis:
     """QGIS Plugin Implementation."""
 
