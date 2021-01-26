79d9ea1b012d20fa7eff6645d04a274f8065e8b4
AbnerErnaniADSFatec <abiner.anjos@gmail.com>
2021-01-26 16:36:28 -0300

    replace wlts to wtss

--- wtss_plugin/dependencies/__init__.py
+++ wtss_plugin/dependencies/__init__.py
@@ -13,8 +13,5 @@
 
 # Exporting control classes to main class plugin
 from .files_export.files_export import FilesExport
-from .version import __version__
 from .wtss_client.wtss_client import wtss
 from .wtss_qgis_controller import Controls, Services
-
-__all__ = ('__version__')
