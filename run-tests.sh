def6127adef8a6a252b1a70e56589491f0992828
AbnerErnaniADSFatec <abiner.anjos@gmail.com>
2021-03-05 16:20:01 -0300

    [#38] Finish pydocstyle integration in run-tests

--- run-tests.sh
+++ run-tests.sh
@@ -1,4 +1,4 @@
-# pydocstyle wtss_plugin setup.py && \
+pydocstyle wtss_plugin/*.py wtss_plugin/dependencies/*.py wtss_plugin/dependencies/files_export/*.py setup.py && \
 isort wtss_plugin setup.py --check-only --diff && \
 check-manifest --ignore ".drone.yml,.readthedocs.yml" && \
 sphinx-build -qnW --color -b doctest wtss_plugin/help/source wtss_plugin/help/_build && \
